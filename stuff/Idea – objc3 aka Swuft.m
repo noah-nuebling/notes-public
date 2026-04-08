987c6a0755cae59f80e434ef18ac344a2b551952
Noah Nuebling <noah.n.public@gmail.com>
2026-04-08 14:11:41 +0200

    [autosync]

--- stuff/Idea – objc3 aka Swuft.m
+++ stuff/Idea – objc3 aka Swuft.m
@@ -678,5 +678,69 @@ arr.[from: a to: b];
             - I'd stick with prefixed identifiers for the stdlib / Foundation -> NSString over String and so on
                 -> Pragmatically, it's fine – greppable, Googlable, still easy-to-type, adds context, only two more characters, still registers as 'one token' in my brain. 
             - 
-
 */
+
+// Claude's version:
+  - (void)fetchAll:(NSArray<NSURL *> *)urls completion:(void(^)(NSArray *))completion {
+      dispatch_group_t group = dispatch_group_create();
+      NSMutableArray *results = [NSMutableArray arrayWithCapacity:urls.count];
+
+      for (NSUInteger i = 0; i < urls.count; i++) {
+          [results addObject:[NSNull null]];
+      }
+
+      [urls enumerateObjectsUsingBlock:^(NSURL *url, NSUInteger idx, BOOL *stop) {
+          dispatch_group_enter(group);
+          [[NSURLSession sharedSession] dataTaskWithURL:url
+              completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+              @synchronized(results) {
+                  results[idx] = data ?: [NSNull null];
+              }
+              dispatch_group_leave(group);
+          }];
+      }];
+
+      dispatch_group_notify(group, dispatch_get_main_queue(), ^{
+          completion(results);
+      });
+  }
+
+// Trying to simplify Swuft-style:
+NSData *[NSArray *] fetchAll: (NSString *[NSArray *urls]) completion: (void (^completion)(NSArray *)) { // Putting the name in the parens along with the type simplifies block args a bit (Update: I removed the completion block arg – so you can't see this anymore) (Update2: Added it back.)|| Not sure about the new `NSData *[NSArray *url]` syntax. Kinda cursed, but I wanted to see what it looks like.
+    
+    NSThread.[detachThreadWithBlock: ^void () { 
+
+        auto group = NSThreadGroup.[new]; // New primitive in the stdlib
+        
+        auto results = @(NSNull.[null] for range(i, urls.count)); // Creates an NSArray. NSArray is mutable, NSMutableArray is deprecated / an alias || range() is a very simple macro I'm already using in objc for some code – not sure it should be part of objc 3? Let's go with it for now.
+
+        for range(i, urls.count) { // Didn't change this too much – just used dot-bracket syntax
+            group.[enter];
+            NSURLSession.[sharedSession].[dataTaskWithURL: urls[i] completionHandler: ^(NSData *data, NSURLResponse *response, NSError *error) {
+                @synchronized(results) results[i] = data ?: NSNull.[null];
+                group.[leave];
+            }];
+        }
+        group.[wait];
+        completion(results);
+    }];
+}
+
+// If you really wanted to make it more concise, you could add NSThreadGroup support into the NSURLSession API. (Like you'd have to to support `async-await` keywords, too)
+//      ... Ok this buys you basically nothing – is this shorter with async-await?
+NSData *[NSArray *] fetchAll: (NSString *[NSArray *urls]) completion: (void (^completion)(NSArray *)) {
+    
+    NSThread.[detachThreadWithBlock: ^void () { 
+        
+        auto results = @(NSNull.[null] for range(i, urls.count)); // Creates an NSArray. NSArray is mutable, NSMutableArray is deprecated / an alias || range() is a very simple macro I'm already using in objc for some code – not sure it should be part of objc 3? Let's go with it for now.
+        
+        auto group = NSThreadGroup.[new];
+        for range(i, urls.count) { // Didn't change this too much – just used dot-bracket syntax
+            NSURLSession.[sharedSession].[dataTaskWithURL: urls[i] threadGroup: group completionHandler: ^(NSData *data, NSURLResponse *response, NSError *error) {
+                @synchronized(results) results[i] = data ?: NSNull.[null];
+            }];
+        }
+        group.[wait];
+        completion(results);
+    }];
+}
