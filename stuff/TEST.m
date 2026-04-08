999b3f7b7d44b5a4ef2bcb17b7fcb8e36655299b
Noah Nuebling <noah.n.public@gmail.com>
2026-04-08 13:51:29 +0200

    [autosync]

--- stuff/TEST.m
+++ stuff/TEST.m
@@ -25,19 +25,22 @@
   }
 
 // Trying to simplify Swuft-style:
-NSData *[NSArray *] fetchAll: (NSString *[NSArray *] urls) { // Putting the name in the parens along with the type simplifies block args a bit (Update: I removed the completion block arg – so you can't see this anymore)|| Not sure about the new `NSData *[NSArray *]` syntax, but I wanted to see what it looks like.
+NSData *[NSArray *] fetchAll: (NSString *[NSArray *] urls) completion: (void ^completion(NSArray *)) { // Putting the name in the parens along with the type simplifies block args a bit (Update: I removed the completion block arg – so you can't see this anymore) (Update2: Added it back.)|| Not sure about the new `NSData *[NSArray *]` syntax, but I wanted to see what it looks like.
     
-    auto group = NSThreadGroup.[new]; // New primitive in the stdlib
-    
-    auto results = @(NSNull.[null] for range(i, urls.count)); // Creates an NSArray. NSArray is mutable, NSMutableArray is deprecated / an alias || range() is a very simple macro I'm already using in objc for some code – not sure it should be part of objc 3? Let's go with it for now.
+    NSThread.[detachThreadWithBlock: ^void () { 
 
-    for range(i, urls.count) { // Didn't change this too much – just used dot-bracket syntax
-        group.[enter];
-        NSURLSession.[sharedSession].[dataTaskWithURL: urls[i] completionHandler: ^(NSData *data, NSURLResponse *response, NSError *error) {
-            @synchronized(results) results[i] = data ?: NSNull.[null];
-            group.[leave];
-        }];
-    }
-    group.[wait];
-    return results; /// The return semantics are simplified to just be blocking – not sure that's a fair comparison? I don't understand when you'd use the original semantics.
+        auto group = NSThreadGroup.[new]; // New primitive in the stdlib
+        
+        auto results = @(NSNull.[null] for range(i, urls.count)); // Creates an NSArray. NSArray is mutable, NSMutableArray is deprecated / an alias || range() is a very simple macro I'm already using in objc for some code – not sure it should be part of objc 3? Let's go with it for now.
+
+        for range(i, urls.count) { // Didn't change this too much – just used dot-bracket syntax
+            group.[enter];
+            NSURLSession.[sharedSession].[dataTaskWithURL: urls[i] completionHandler: ^(NSData *data, NSURLResponse *response, NSError *error) {
+                @synchronized(results) results[i] = data ?: NSNull.[null];
+                group.[leave];
+            }];
+        }
+        group.[wait];
+        completion(results);
+    }];
 }
